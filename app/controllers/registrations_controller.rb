d8ce234f7fef2006dfd67e123f9f93a30f06aa84
mikami akito <mikamiakito@mikaminoMacBook-Pro.local>
2025-01-19 18:23:48 +0900

    新規登録機能実装

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,24 @@
+# app/controllers/registrations_controller.rb
 class RegistrationsController < ApplicationController
+  allow_unauthenticated_access only: [:new, :create]
+
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      start_new_session_for(@user)
+      redirect_to after_authentication_url, notice: "Welcome! You have signed up successfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email_address, :password, :password_confirmation)
   end
 end
